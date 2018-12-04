21d44232b90036bcb29dee6438db4d189d152f3a
root <root@virtlab420.virt.lab.eng.bos.redhat.com>
2018-12-04 05:49:40 -0500

    Change
    Signed-off-by: root <root@virtlab420.virt.lab.eng.bos.redhat.com>

--- include/linux/page_hinting.h
+++ include/linux/page_hinting.h
@@ -1,7 +1,7 @@
 #include <linux/smpboot.h>
 
 //#define MAX_FGPT_ENTRIES	250000
-#define MAX_FGPT_ENTRIES	25000
+#define MAX_FGPT_ENTRIES	1000
 /*
  * hypervisor_pages - It is a dummy structure passed with the hypercall.
  * @pfn - page frame number for the page which is to be freed.
