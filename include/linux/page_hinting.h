19e69ce2fbe0b852c5e667ca86f8df1ad895bdb4
Nitesh Narayan Lal <nitesh@redhat.com>
2019-03-06 09:02:51 -0500

    KVM: Guest free page hinting support
    This patch adds the following:
    1. Functional skeleton for the guest implementation. It enables the
    guest to maintain the PFN of head buddy free pages of order
    FREE_PAGE_HINTING_MIN_ORDER (currently defined as MAX_ORDER - 1)
    in a per-cpu array.
    Guest uses guest_free_page_enqueue() to enqueue the free pages post buddy
    merging to the above mentioned per-cpu array.
    guest_free_page_try_hinting() is used to initiate hinting operation once
    the collected entries of the per-cpu array reaches HINTING_THRESHOLD (128).
    For now guest_free_page_hinting() just resets the array index to continue
    capturing of the freed pages.
    2. Enables the support for x86 architecture.
    
    Signed-off-by: Nitesh Narayan Lal <nitesh@redhat.com>

---
+++ include/linux/page_hinting.h
@@ -0,0 +1,15 @@
+#include <linux/gfp.h>
+/*
+ * Size of the array which is used to store the freed pages is defined by
+ * MAX_FGPT_ENTRIES.
+ */
+#define MAX_FGPT_ENTRIES	256
+/*
+ * Threshold value after which hinting needs to be initiated on the captured
+ * free pages.
+ */
+#define HINTING_THRESHOLD	128
+#define FREE_PAGE_HINTING_MIN_ORDER	(MAX_ORDER - 1)
+
+void guest_free_page_enqueue(struct page *page, int order);
+void guest_free_page_try_hinting(void);
