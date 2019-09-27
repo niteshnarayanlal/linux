ab8e2a4a6a405d270e0bf8114369c4d4fb562431
Nitesh Narayan Lal <nitesh@redhat.com>
2019-09-27 12:03:44 -0400

    mm: page_reporting: core infrastructure
    This patch introduces the core infrastructure for free page reporting in
    virtual environments. It enables the kernel to track the free pages which
    can be reported to its hypervisor so that the hypervisor could
    free and reuse that memory as per its requirement.
    
    While the pages are getting processed in the hypervisor (e.g.,
    via MADV_DONTNEED), the guest must not use them, otherwise, data loss
    would be possible. To avoid such a situation, these pages are
    temporarily removed from the buddy. The amount of pages removed
    temporarily from the buddy is governed by the backend(virtio-balloon
    in our case).
    
    To efficiently identify free pages that can to be reported to the
    hypervisor, bitmaps in a coarse granularity are used. Only fairly big
    chunks are reported to the hypervisor - especially, to not break up THP
    in the hypervisor - "MAX_ORDER - 2" on x86, and to save space. The bits
    in the bitmap are an indication whether a page *might* be free, not a
    guarantee. A new hook after buddy merging sets the bits.
    
    Bitmap and its respective fields are maintained in a structure whose RCU
    protected pointer is maintained on a per-zone basis. A workqueue
    asynchronously processes the bitmaps, trying to isolate and report pages
    that are still free.
    
    The backend (virtio-balloon) is responsible for reporting these batched
    pages to the host synchronously. Once reporting/freeing is complete,
    isolated pages are returned back to the buddy.
    
    Signed-off-by: Nitesh Narayan Lal <nitesh@redhat.com>

---
+++ mm/page_reporting.c
@@ -0,0 +1,394 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Page reporting core infrastructure to enable a VM to report free pages to its
+ * hypervisor.
+ *
+ * Copyright Red Hat, Inc. 2019
+ *
+ * Author(s): Nitesh Narayan Lal <nitesh@redhat.com>
+ */
+
+#include <linux/mm.h>
+#include <linux/slab.h>
+#include <linux/page_reporting.h>
+#include <linux/scatterlist.h>
+#include <linux/page-isolation.h>
+#include "internal.h"
+
+static struct page_reporting_config __rcu *page_reporting_conf __read_mostly;
+static DEFINE_MUTEX(page_reporting_mutex);
+
+static void return_isolated_page(struct zone *zone,
+				 struct page_reporting_config *phconf)
+{
+	struct scatterlist *sg = phconf->sg;
+
+	spin_lock(&zone->lock);
+	do {
+		__return_isolated_page(zone, sg_page(sg));
+	} while (!sg_is_last(sg++));
+	spin_unlock(&zone->lock);
+}
+
+static void bitmap_set_bit(struct page *page,
+			   struct zone_reporting_bitmap *rbitmap)
+{
+	unsigned long pfn, bitnr = 0;
+
+	if (unlikely(!rbitmap->bitmap))
+		return;
+
+	pfn = page_to_pfn(page);
+	bitnr = (pfn - rbitmap->base_pfn) >> PAGE_REPORTING_MIN_ORDER;
+
+	/* set bit if it is not already set and is a valid bit */
+	if (bitnr < rbitmap->nbits &&
+	    !test_and_set_bit(bitnr, rbitmap->bitmap &&
+	    bitnr < rbitmap->last_scanned_bit)
+		/*
+		 * Increment free_pages only if the freed page belong to the
+		 * already scanned bitmap section.
+		 */
+		atomic_inc(&rbitmap->free_pages);
+}
+
+static int process_free_page(struct page *page,
+			     struct page_reporting_config *phconf, int count)
+{
+	int order, ret = 0;
+
+	order = page_private(page);
+	ret = __isolate_free_page(page, order);
+
+	if (ret) {
+		/*
+		 * Preserving order for reuse while releasing the pages back
+		 * to the buddy.
+		 */
+		set_page_private(page, order);
+
+		sg_set_page(&phconf->sg[count++], page,
+			    PAGE_SIZE << order, 0);
+	}
+
+	return count;
+}
+
+/**
+ * scan_zone_bitmap - scans the bitmap for the requested zone.
+ * @phconf: page reporting configuration object initialized by the backend.
+ * @rbitmap: reporting bitmap object carrying zone's bitmap and required fields.
+ * @zone: zone for which page reporting is requested.
+ *
+ * For every page marked in the bitmap it checks if it is still free if so it
+ * isolates and adds them to a scatterlist. As soon as the number of isolated
+ * pages reach the threshold set by the backend, they are reported to the
+ * hypervisor by the backend. Once the hypervisor responds after processing
+ * they are returned back to the buddy for reuse.
+ */
+static void scan_zone_bitmap(struct page_reporting_config *phconf,
+			     struct zone_reporting_bitmap *rbitmap,
+			     struct zone *zone)
+{
+	unsigned long setbit;
+	struct page *page;
+	int count = 0;
+
+	sg_init_table(phconf->sg, phconf->max_pages);
+
+	for_each_set_bit(setbit, rbitmap->bitmap, rbitmap->nbits) {
+		rbitmap->last_scanned_bit = setbit;
+		/* Process only if the page is still online */
+		page = pfn_to_online_page((setbit << PAGE_REPORTING_MIN_ORDER) +
+					  rbitmap->base_pfn);
+		if (!page) {
+			clear_bit(setbit, rbitmap->bitmap);
+			continue;
+		}
+
+		spin_lock(&zone->lock);
+
+		/* Ensure page is still free and can be processed */
+		if (PageBuddy(page) && !is_migrate_isolate_page(page) &&
+		    page_private(page) >= PAGE_REPORTING_MIN_ORDER)
+			count = process_free_page(page, phconf, count);
+
+		spin_unlock(&zone->lock);
+		/* Page has been processed, adjust its bit and zone counter */
+		clear_bit(setbit, rbitmap->bitmap);
+
+		if (count == phconf->max_pages) {
+			/* Report isolated pages to the hypervisor */
+			phconf->report(phconf, count);
+
+			/* Return processed pages back to the buddy */
+			return_isolated_page(zone, phconf);
+
+			/* Reset for next reporting */
+			sg_init_table(phconf->sg, phconf->max_pages);
+			count = 0;
+		}
+	}
+	/*
+	 * If the number of isolated pages does not meet the max_pages
+	 * threshold, we would still prefer to report them as we have already
+	 * isolated them.
+	 */
+	if (count) {
+		sg_mark_end(&phconf->sg[count - 1]);
+		phconf->report(phconf, count);
+
+		return_isolated_page(zone, phconf);
+	}
+
+	atomic_dec(&phconf->refcnt);
+	rbitmap->last_scanned_bit = rbitmap->nbits;
+}
+
+/**
+ * page_reporting_wq - checks the number of free_pages in all the zones and
+ * invokes a request to scan the respective bitmap if free_pages reaches or
+ * exceeds the threshold specified by the backend.
+ */
+static void page_reporting_wq(struct work_struct *work)
+{
+	struct page_reporting_config *phconf =
+		container_of(work, struct page_reporting_config,
+			     reporting_work);
+	struct zone *zone = first_online_pgdat()->node_zones;
+	struct zone_reporting_bitmap *rbitmap;
+	int free_pages;
+
+	do {
+		if (populated_zone(zone)) {
+			/*
+			 * A newly enqueued/ongoing job will be canceled before
+			 * the rcu protected zone_reporting_bitmap pointer
+			 * object is freed. Hence, it should be safe to access
+			 * it here without a read lock.
+			 */
+			rbitmap = rcu_dereference(zone->reporting_bitmap);
+			free_pages = atomic_read(&rbitmap->free_pages);
+
+			if (rbitmap && free_pages >= phconf->max_pages) {
+				atomic_set(&rbitmap->free_pages, 0);
+				scan_zone_bitmap(phconf, rbitmap, zone);
+			}
+		}
+		/* Move to the next zone if possible else start over */
+		zone = next_zone(zone) ? : first_online_pgdat()->node_zones;
+		/*
+		 * We should continue checking the zones for possible reporting
+		 * as long as the refcnt has not reached to zero.
+		 */
+	} while (atomic_read(&phconf->refcnt));
+	__clear_bit(PAGE_REPORTING_ACTIVE, &phconf->flags);
+}
+
+/**
+ * __page_reporting_enqueue - tracks the freed page in the respective zone's
+ * bitmap and enqueues a new page reporting job to the workqueue if possible.
+ */
+void __page_reporting_enqueue(struct page *page)
+{
+	struct page_reporting_config *phconf;
+	struct zone_reporting_bitmap *rbitmap;
+	struct zone *zone;
+
+	rcu_read_lock();
+	/*
+	 * We should not process this page if either page reporting is not
+	 * yet completely enabled or it has been disabled by the backend.
+	 */
+	phconf = rcu_dereference(page_reporting_conf);
+	if (!phconf)
+		goto out;
+
+	zone = page_zone(page);
+	rbitmap = rcu_dereference(zone->reporting_bitmap);
+	if (!rbitmap)
+		goto out;
+
+	bitmap_set_bit(page, rbitmap);
+
+	/*
+	 * We should not enqueue a job if we don't have enough free pages in
+	 * the zone or if this zone has an ongoing page reporting request.
+	 */
+	if (atomic_read(&rbitmap->free_pages) < phconf->max_pages ||
+	    test_bit(PAGE_REPORTING_ACTIVE, &phconf->flags))
+		goto out;
+
+	__set_bit(PAGE_REPORTING_ACTIVE, &phconf->flags);
+
+	if (!atomic_fetch_inc(&phconf->refcnt))
+		schedule_work(&phconf->reporting_work);
+
+out:
+	rcu_read_unlock();
+}
+
+/**
+ * zone_reporting_cleanup - resets the page reporting fields and free the
+ * bitmap for all the initialized zones.
+ */
+static void zone_reporting_cleanup(void)
+{
+	struct zone *zone;
+
+	for_each_populated_zone(zone) {
+		struct zone_reporting_bitmap *rbitmap;
+
+		rbitmap = rcu_dereference(zone->reporting_bitmap);
+		/*
+		 * Bitmap fields may not necessarily be allocated for all the
+		 * populated zones. Specifically when an allocation fails before
+		 * completely initializing the last zone.
+		 */
+		if (!rbitmap)
+			continue;
+		RCU_INIT_POINTER(zone->reporting_bitmap, NULL);
+		synchronize_rcu();
+
+		if (rbitmap->bitmap) {
+			bitmap_free(rbitmap->bitmap);
+			rbitmap->bitmap = NULL;
+		}
+		atomic_set(&rbitmap->free_pages, 0);
+		kfree(rbitmap);
+	}
+}
+
+static int zone_bitmap_alloc(struct zone_reporting_bitmap *rbitmap)
+{
+	unsigned long bitmap_size, pages;
+
+	pages = rbitmap->end_pfn - rbitmap->base_pfn;
+	bitmap_size = (pages >> PAGE_REPORTING_MIN_ORDER) + 1;
+
+	if (!bitmap_size)
+		return 0;
+
+	rbitmap->bitmap = bitmap_zalloc(bitmap_size, GFP_KERNEL);
+	if (!rbitmap->bitmap)
+		return -ENOMEM;
+
+	rbitmap->nbits = bitmap_size;
+	/*
+	 * Each zone increments free_pages only if it belongs to a bit position
+	 * which has already been scanned.
+	 * Initially we set the index to the end so that the zone could
+	 * increment free_pages and generate reporting request.
+	 */
+	rbitmap->last_scanned_bit = bitmap_size;
+
+	return 0;
+}
+
+/**
+ * zone_reporting_init - for each zone initializes the page reporting fields
+ * and allocates the respective bitmap.
+ *
+ * This function returns 0 on successful initialization, -ENOMEM otherwise.
+ */
+static int zone_reporting_init(void)
+{
+	struct zone *zone;
+	int ret = 0;
+
+	for_each_populated_zone(zone) {
+		/*
+		 * Zones eg. ZONE_DEVICE with pages not in the buddy are never
+		 * considered populated.
+		 */
+		struct zone_reporting_bitmap *rbitmap;
+
+		rbitmap = kmalloc(sizeof(*rbitmap), GFP_KERNEL);
+		if (!rbitmap) {
+			zone_reporting_cleanup();
+			return -ENOMEM;
+		}
+
+		/*
+		 * Preserve start and end PFN values in case they change due
+		 * to memory hotplug.
+		 */
+		spin_lock(&zone->lock);
+		rbitmap->base_pfn = zone->zone_start_pfn;
+		rbitmap->end_pfn = zone_end_pfn(zone);
+		spin_unlock(&zone->lock);
+
+		atomic_set(&rbitmap->free_pages, 0);
+
+		ret = zone_bitmap_alloc(rbitmap);
+		if (ret < 0) {
+			zone_reporting_cleanup();
+			return ret;
+		}
+		rcu_assign_pointer(zone->reporting_bitmap, rbitmap);
+	}
+
+	return 0;
+}
+
+void page_reporting_disable(struct page_reporting_config *phconf)
+{
+	mutex_lock(&page_reporting_mutex);
+
+	if (rcu_access_pointer(page_reporting_conf) != phconf)
+		return;
+
+	RCU_INIT_POINTER(page_reporting_conf, NULL);
+	synchronize_rcu();
+
+	/* Cancel any pending reporting request */
+	cancel_work_sync(&phconf->reporting_work);
+
+	/* Free the scatterlist used for isolated pages */
+	kfree(phconf->sg);
+	phconf->sg = NULL;
+
+	/* Cleanup the bitmaps and old tracking data */
+	zone_reporting_cleanup();
+
+	mutex_unlock(&page_reporting_mutex);
+}
+EXPORT_SYMBOL_GPL(page_reporting_disable);
+
+int page_reporting_enable(struct page_reporting_config *phconf)
+{
+	int ret = 0;
+
+	mutex_lock(&page_reporting_mutex);
+
+	/* check if someone is already using page reporting*/
+	if (rcu_access_pointer(page_reporting_conf)) {
+		ret = -EBUSY;
+		goto out;
+	}
+
+	/* allocate scatterlist to hold isolated pages */
+	phconf->sg = kcalloc(phconf->max_pages, sizeof(*phconf->sg),
+			     GFP_KERNEL);
+	if (!phconf->sg) {
+		ret = -ENOMEM;
+		goto out;
+	}
+
+	/* initialize each zone's bitmap required for page reporting */
+	ret = zone_reporting_init();
+	if (ret < 0) {
+		kfree(phconf->sg);
+		goto out;
+	}
+
+	INIT_WORK(&phconf->reporting_work, page_reporting_wq);
+
+	/* assign the configuration object provided by the backend */
+	rcu_assign_pointer(page_reporting_conf, phconf);
+
+out:
+	mutex_unlock(&page_reporting_mutex);
+	return ret;
+}
+EXPORT_SYMBOL_GPL(page_reporting_enable);
