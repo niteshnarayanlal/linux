1c522e37c116e89875eb71da1429701b72ad05cc
Nitesh Narayan Lal <nitesh@redhat.com>
2019-10-01 11:17:01 -0400

    mm: page_reporting: core infrastructure
    This patch introduces the core infrastructure for free page reporting in
    virtual environments. It enables the kernel to track the free pages which
    can be reported to its hypervisor so that the hypervisor could
    free and reuse that memory as per its requirement.
    
    While the pages are getting processed in the hypervisor (e.g.,
    via MADV_DONTNEED), the guest must not use them, otherwise, data loss
    would be possible. To avoid such a situation, these pages are
    temporarily removed from the buddy. The amount of pages removed
    temporarily from the buddy is governed by the backend(virtio-balloon
    in our case).
    
    To efficiently identify free pages that can to be reported to the
    hypervisor, bitmaps in a coarse granularity are used. Only fairly big
    chunks are reported to the hypervisor - especially, to not break up THP
    in the hypervisor - "MAX_ORDER - 2" on x86, and to save space. The bits
    in the bitmap are an indication whether a page *might* be free, not a
    guarantee. A new hook after buddy merging sets the bits.
    
    Bitmap and its respective fields are maintained in a structure whose RCU
    protected pointer is maintained on a per-zone basis. A workqueue
    asynchronously processes the bitmaps, trying to isolate and report pages
    that are still free.
    
    The backend (virtio-balloon) is responsible for reporting these batched
    pages to the host synchronously. Once reporting/freeing is complete,
    isolated pages are returned back to the buddy.
    
    Signed-off-by: Nitesh Narayan Lal <nitesh@redhat.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -68,6 +68,7 @@
 #include <linux/lockdep.h>
 #include <linux/nmi.h>
 #include <linux/psi.h>
+#include <linux/page_reporting.h>
 
 #include <asm/sections.h>
 #include <asm/tlbflush.h>
@@ -904,7 +905,7 @@ compaction_capture(struct capture_control *capc, struct page *page,
 static inline void __free_one_page(struct page *page,
 		unsigned long pfn,
 		struct zone *zone, unsigned int order,
-		int migratetype)
+		int migratetype, bool needs_reporting)
 {
 	unsigned long combined_pfn;
 	unsigned long uninitialized_var(buddy_pfn);
@@ -1007,7 +1008,8 @@ done_merging:
 				migratetype);
 	else
 		add_to_free_area(page, &zone->free_area[order], migratetype);
-
+	if (needs_reporting)
+		page_reporting_enqueue(page, order);
 }
 
 /*
@@ -1318,7 +1320,7 @@ static void free_pcppages_bulk(struct zone *zone, int count,
 		if (unlikely(isolated_pageblocks))
 			mt = get_pageblock_migratetype(page);
 
-		__free_one_page(page, page_to_pfn(page), zone, 0, mt);
+		__free_one_page(page, page_to_pfn(page), zone, 0, mt, true);
 		trace_mm_page_pcpu_drain(page, 0, mt);
 	}
 	spin_unlock(&zone->lock);
@@ -1327,14 +1329,14 @@ static void free_pcppages_bulk(struct zone *zone, int count,
 static void free_one_page(struct zone *zone,
 				struct page *page, unsigned long pfn,
 				unsigned int order,
-				int migratetype)
+				int migratetype, bool needs_reporting)
 {
 	spin_lock(&zone->lock);
 	if (unlikely(has_isolate_pageblock(zone) ||
 		is_migrate_isolate(migratetype))) {
 		migratetype = get_pfnblock_migratetype(page, pfn);
 	}
-	__free_one_page(page, pfn, zone, order, migratetype);
+	__free_one_page(page, pfn, zone, order, migratetype, needs_reporting);
 	spin_unlock(&zone->lock);
 }
 
@@ -1424,7 +1426,7 @@ static void __free_pages_ok(struct page *page, unsigned int order)
 	migratetype = get_pfnblock_migratetype(page, pfn);
 	local_irq_save(flags);
 	__count_vm_events(PGFREE, 1 << order);
-	free_one_page(page_zone(page), page, pfn, order, migratetype);
+	free_one_page(page_zone(page), page, pfn, order, migratetype, true);
 	local_irq_restore(flags);
 }
 
@@ -2198,6 +2200,29 @@ struct page *__rmqueue_smallest(struct zone *zone, unsigned int order,
 	return NULL;
 }
 
+#ifdef CONFIG_PAGE_REPORTING
+/**
+ * return_isolated_page - returns a reported page back to the buddy.
+ * @zone: zone from where the page was isolated.
+ * @page: page which will be returned.
+ */
+void __return_isolated_page(struct zone *zone, struct page *page)
+{
+	unsigned int order, mt;
+	unsigned long pfn;
+
+	/* zone lock should be held when this function is called */
+	lockdep_assert_held(&zone->lock);
+
+	pfn = page_to_pfn(page);
+	mt = get_pfnblock_migratetype(page, pfn);
+
+	order = page_private(page);
+	set_page_private(page, 0);
+
+	__free_one_page(page, pfn, zone, order, mt, false);
+}
+#endif /* CONFIG_PAGE_REPORTING */
 
 /*
  * This array describes the order lists are fallen back to when
@@ -3031,7 +3056,7 @@ static void free_unref_page_commit(struct page *page, unsigned long pfn)
 	 */
 	if (migratetype >= MIGRATE_PCPTYPES) {
 		if (unlikely(is_migrate_isolate(migratetype))) {
-			free_one_page(zone, page, pfn, 0, migratetype);
+			free_one_page(zone, page, pfn, 0, migratetype, true);
 			return;
 		}
 		migratetype = MIGRATE_MOVABLE;
